ab06046766ce13163fbae2fd0a9deeb81fa63a25
svetlyobg <swk_12@abv.bg>
2017-06-29 12:52:40 +0300

    Add files via upload

---
+++ approval-template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+<body>
+    <?php
+    #PHP & MySQL - юни
+        $name = 'Svetlozar Kosev';
+        $courseName = "PHP";
+        $template = "We are very happy that you joined our training program for the course named: ";
+
+        echo $name.", ".$template.$courseName;
+     ?>
+</body>
+</html>
