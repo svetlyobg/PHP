80c46639ec17e829da37ea3767aa7f50a6805e1f
DESKTOP-53R6RMD\Svetlozar <swk_12@abv.bg>
2017-06-29 15:13:09 +0300

    оператори

---
+++ SoftUni/1. Въведение/2.0.operators.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    #https://www.youtube.com/watch?v=PQk4gC8EarU
+
+    $a = 5;
+    // echo $a;
+    // echo "<br/> \n";echo "<br/> \n";
+    // $a += 3;
+    // print($a)
+    echo $a++;
+    echo $a;
+    echo ++$a;
+    echo $a;
+
+
+    ?>
+</body>
+</html>
