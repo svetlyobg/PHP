a0ecf1c689ccd23dcd2902a880a0bc4acaacd652
DESKTOP-53R6RMD\Svetlozar <swk_12@abv.bg>
2017-06-29 13:26:14 +0300

    variables.php
    showing formatted string w/o a dot

---
+++ variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    #https://softuni.bg/trainings/resources/video/1236/video-vyvedenie-v-php-chast-i-php-mysql-jun-2014
+        $person = "Batman";
+        $quality = "Awsome";
+        echo "$person is $quality";
+    ?>
+</body>
+</html>
