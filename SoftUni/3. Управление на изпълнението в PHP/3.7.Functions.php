f36dfa3731a193bcb836802545d71dd545ac17bf
DESKTOP-53R6RMD\Svetlozar <swk_12@abv.bg>
2017-06-30 10:55:09 +0300

    Функции

---
+++ SoftUni/3. Управление на изпълнението в PHP/3.7.Functions.php
@@ -0,0 +1,12 @@
+<?php
+#"<br>\n";
+
+function calcRectangleArea($sideA,$sideB)
+{
+  return $sideA*$sideB;
+}
+echo calcRectangleArea(5,6) . "\n";
+echo calcRectangleArea(3,2) . "\n";
+echo calcRectangleArea(10,20) . "\n";
+
+ ?>
